fee6a13a7616c14503f9d8fe537737621a5f4b28
HungLuke <108488073+Luuphuochung2210@users.noreply.github.com>
2022-07-09 18:42:35 +0700

    Add files via upload

--- front-end/src/components/Layout/MainPage.jsx
+++ front-end/src/components/Layout/MainPage.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import Menu from './Menu'
 import Navbar from './Navbar'
 import "./mainpage.css"
+
 const MainPage = ({children}) => {
   return (
     <div className='mainpage'>
       <Menu></Menu>
       <div id='main' className='main'>
         <Navbar/>
-        <div className="p-5">
-          {children}
-        </div>
-        
+        {children}
       </div>
-      
     </div>
   )
 }
